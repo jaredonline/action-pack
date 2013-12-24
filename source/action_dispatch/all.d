126e0fcc12e38f3c956fa95a556cbcc668468784
Jared McFarland <jared.online@gmail.com>
2013-12-23 18:10:28 -0800

    renaming project to action-pack; renaming action_pack module to action_dispatch

---
+++ source/action_dispatch/all.d
@@ -0,0 +1,7 @@
+module action_dispatch.all;
+
+public import vibe.d;
+public import action_dispatch.controller;
+public import action_dispatch.namespace;
+public import action_dispatch.router;
+public import action_dispatch.route;
